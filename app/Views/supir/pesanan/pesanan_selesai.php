5637e536125d7993eff9cfc1382820485a2b0620
Irvandra <83211382+Irvandra@users.noreply.github.com>
2022-10-18 15:31:35 +0700

    Membuat view supir

--- app/Views/supir/pesanan/pesanan_selesai.php
+++ app/Views/supir/pesanan/pesanan_selesai.php
@@ -23,10 +23,11 @@ echo $this->section('content');
 		<div class="container-fluid">
 			<div class="row">
 				<div class="col-12">
-					<table class="table table-striped table-dark">
+					<table class="table table-striped table-bordered border shadow">
 						<thead>
 							<tr class="table">
 								<th scope="col">No</th>
+								<th scope="col">Action</th>
 								<th scope="col">Id</th>
 								<th scope="col">Jumlah</th>
 								<th scope="col">Total</th>
@@ -38,6 +39,12 @@ echo $this->section('content');
 						<tbody>
 							<tr>
 								<td scope="row">1</td>
+								<td style="text-align: center;">
+									<!-- detail button -->
+									<a class="btn btn-info btn-sm" href="<?= base_url('/supir/detail_pesanan') ?>" role="button">
+										<ion-icon name="search-outline"></ion-icon>
+									</a>
+								</td>
 								<td>ID7</td>
 								<td>10</td>
 								<td>Rp. 100000</td>
@@ -47,6 +54,12 @@ echo $this->section('content');
 							</tr>
 							<tr>
 								<td scope="row">2</td>
+								<td style="text-align: center;">
+									<!-- detail button -->
+									<a class="btn btn-info btn-sm" href="<?= base_url('/supir/detail_pesanan') ?>" role="button">
+										<ion-icon name="search-outline"></ion-icon>
+									</a>
+								</td>
 								<td>ID8</td>
 								<td>5</td>
 								<td>Rp. 50000</td>
@@ -56,6 +69,12 @@ echo $this->section('content');
 							</tr>
 							<tr>
 								<td scope="row">3</td>
+								<td style="text-align: center;">
+									<!-- detail button -->
+									<a class="btn btn-info btn-sm" href="<?= base_url('/supir/detail_pesanan') ?>" role="button">
+										<ion-icon name="search-outline"></ion-icon>
+									</a>
+								</td>
 								<td>ID9</td>
 								<td>5</td>
 								<td>Rp. 50000</td>
