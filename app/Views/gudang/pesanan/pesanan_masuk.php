e7cd426c383eb6179abd1ef33a4f3c887b6497ac
aribyusronha <arib.yusronhamdani2078@students.unila.ac.id>
2022-11-22 14:51:27 +0700

    Finishing (2)

--- app/Views/gudang/pesanan/pesanan_masuk.php
+++ app/Views/gudang/pesanan/pesanan_masuk.php
@@ -15,58 +15,63 @@ echo $this->section('content');
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col-12">
-				<table class="table table-striped table-bordered border shadow">
-					<thead>
-						<tr class="table" style="text-align: center;">
-							<th scope="col">No</th>
-							<th scope="col">Id Pesanan</th>
-							<th scope="col">Nama Toko</th>
-							<th scope="col">Tanggal</th>
-							<th scope="col">Alamat</th>
-							<th scope="col">Action</th>
+				<div class="card">
+					<div class="card-body">
 
-						</tr>
-					</thead>
-					<tbody>
-						<?php
-						if ($pesanan == null) {
-						?>
-							<tr class="text-center">
-								<td colspan="7">Tidak ada pesanan yang masuk</td>
-							</tr>
-							<?php
-						} else {
-							$i = 1;
-							foreach ($pesanan as $p) {
-							?>
+						<table class="table table-striped table-bordered border shadow">
+							<thead>
+								<tr class="table" style="text-align: center;">
+									<th scope="col">No</th>
+									<th scope="col">Id Pesanan</th>
+									<th scope="col">Nama Toko</th>
+									<th scope="col">Tanggal</th>
+									<th scope="col">Alamat</th>
+									<th scope="col">Action</th>
 
-								<tr style="text-align: center;">
-									<td scope="row"><?= $i++ ?></td>
-									<td><?= $p->receipt ?></td>
-									<td><?= $p->nama_toko ?></td>
-									<td><?= $p->tanggal ?></td>
-									<td><?= $p->alamat ?></td>
-									<td>
-										<!-- prosses button -->
-										<a class="btn btn-success btn-sm" href="<?= base_url('gudang/proses/' . $p->id_pesanan) ?>" role="button">
-											<i class="bi bi-clipboard-check"></i>
-										</a>
-										<!-- detail button -->
-										<a class="btn btn-primary btn-sm" href="<?= base_url('gudang/detail_pesanan/' . $p->id_pesanan) ?>" role="button">
-											<i class="bi bi-file-text"></i>
-										</a>
-										<!-- reject button -->
-										<a class="btn btn-danger btn-sm" href="<?= base_url('gudang/reject/' . $p->id_pesanan) ?>" role="button" onclick="return confirm('Yakin Ingin Membatalkan Pesanan?')">
-											<i class="bi bi-clipboard-x"></i>
-										</a>
-									</td>
 								</tr>
-						<?php
-							}
-						}
-						?>
-					</tbody>
-				</table>
+							</thead>
+							<tbody>
+								<?php
+								if ($pesanan == null) {
+								?>
+									<tr class="text-center">
+										<td colspan="7">Tidak ada pesanan yang masuk</td>
+									</tr>
+									<?php
+								} else {
+									$i = 1;
+									foreach ($pesanan as $p) {
+									?>
+
+										<tr style="text-align: center;">
+											<td scope="row"><?= $i++ ?></td>
+											<td><?= $p->receipt ?></td>
+											<td><?= $p->nama_toko ?></td>
+											<td><?= $p->tanggal ?></td>
+											<td><?= $p->alamat ?></td>
+											<td>
+												<!-- prosses button -->
+												<a class="btn btn-success btn-sm" href="<?= base_url('gudang/proses/' . $p->id_pesanan) ?>" role="button">
+													<i class="bi bi-clipboard-check"></i>
+												</a>
+												<!-- detail button -->
+												<a class="btn btn-primary btn-sm" href="<?= base_url('gudang/detail_pesanan/' . $p->id_pesanan) ?>" role="button">
+													<i class="bi bi-file-text"></i>
+												</a>
+												<!-- reject button -->
+												<a class="btn btn-danger btn-sm" href="<?= base_url('gudang/reject/' . $p->id_pesanan) ?>" role="button" onclick="return confirm('Yakin Ingin Membatalkan Pesanan?')">
+													<i class="bi bi-clipboard-x"></i>
+												</a>
+											</td>
+										</tr>
+								<?php
+									}
+								}
+								?>
+							</tbody>
+						</table>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
